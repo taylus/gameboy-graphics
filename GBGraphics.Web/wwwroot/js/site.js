0e2044d90da1b33a62c34d05ffffbe10098db2e1
taylus <brandon@legacy.codes>
2019-08-04 12:02:16 -0500

    Add palette selection to frontend

--- GBGraphics.Web/wwwroot/js/site.js
+++ GBGraphics.Web/wwwroot/js/site.js
@@ -1,15 +1,24 @@
-﻿/* jshint browser: true, devel: true */
+﻿/* jshint esversion: 6, browser: true, devel: true */
 document.addEventListener("DOMContentLoaded", function () {
     "use strict";
+
+    //colorize the image when the input file changes
     var input = document.querySelector("input[type=file]");
     input.addEventListener("change", function () {
-        var formData = new FormData();
-        formData.append("img", input.files[0]);
-        //TODO: let user select color palette ala https://codepen.io/taylus/pen/OKxqNM
-        formData.append("colors", "#e0f8d0");
-        formData.append("colors", "#88c070");
-        formData.append("colors", "#346856");
-        formData.append("colors", "#081820");
+        colorizeImage();
+    });
+
+    //or when the palette changes while an input file is selected
+    document.querySelectorAll("input[type=radio]").forEach(function (radio) {
+        radio.addEventListener("change", function () {
+            colorizeImage();
+        }, false);
+    });
+
+    function colorizeImage() {
+        var file = input.files[0];
+        if (!file) return;
+
         var screen = document.getElementById("screen");
         if (screen) {
             screen.src = "/img/loading-spinner.gif";
@@ -17,11 +26,66 @@ document.addEventListener("DOMContentLoaded", function () {
         }
         fetch("", {
             method: "POST",
-            body: formData
+            body: buildRequest()
         }).then(function (response) {
             return response.blob();
         }).then(function (response) {
             if (screen) screen.src = URL.createObjectURL(response);
         });
+
+        function buildRequest() {
+            var formData = new FormData();
+            formData.append("img", file);
+
+            var palette = getSelectedColors();
+            palette.forEach(function (color) {
+                formData.append("colors", color);
+            });
+
+            return formData;
+        }
+    }
+
+    //fill color palette labels with their (hidden) color input's color
+    //since <input type="color"> looks gross and is largely unstylable
+    //https://stackoverflow.com/a/26086382/7512368
+    document.querySelectorAll("label.color-swatch").forEach(function (label) {
+        var color = label.querySelector("input[type=color]").value;
+        label.style.backgroundColor = color;
     });
+
+    document.querySelectorAll("input[type=color]").forEach(function (input) {
+        //update the label's background color when a color input changes
+        input.addEventListener("change", function () {
+            this.parentNode.style.backgroundColor = this.value;
+            colorizeImage();
+        }, false);
+
+        //select the "use custom colors" radio button when a color input is clicked
+        input.addEventListener("click", function () {
+            document.getElementById("custom").checked = true;
+            colorizeImage();
+        }, false);
+    });
+
+    function getSelectedColors() {
+        var selectedPalette = document.querySelector("input[name=palette]:checked").value;
+        var label = document.querySelector("label[for=" + selectedPalette + "]");
+        if (selectedPalette !== "custom") {
+            var spans = Array.from(label.querySelectorAll("span"));
+            return spans.map((div) => rgbToHex(div.style.backgroundColor));
+        }
+        else {
+            var inputs = Array.from(label.querySelectorAll("input[type=color]"));
+            return inputs.map((input) => input.value);
+        }
+    }
+
+    function rgbToHex(rgb) {
+        rgb = rgb.match(/^rgba?[\s+]?\([\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?/i);
+        return (rgb && rgb.length === 4) ? "#" +
+            ("0" + parseInt(rgb[1], 10).toString(16)).slice(-2) +
+            ("0" + parseInt(rgb[2], 10).toString(16)).slice(-2) +
+            ("0" + parseInt(rgb[3], 10).toString(16)).slice(-2) : '';
+    }
 }, false);
